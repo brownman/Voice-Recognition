40cdcdc1e79fd08610681ac1e1d5f997c427fac7
Josef Bacik <josef@toxicpanda.com>
2010-03-02 19:23:34 -0500

    - add new wav files of things to compare with - fix file.c to take a file argument - divide all the entries by the min/max to scale everything down properly

--- file.c
+++ file.c
@@ -7,6 +7,11 @@
 #include <fftw3.h>
 #include <math.h>
 
+struct frequency {
+	double magnitude;
+	int sample;
+};
+
 int main(int argc, char **argv)
 {
 	SNDFILE *file;
@@ -24,10 +29,17 @@ int main(int argc, char **argv)
 	fftw_complex *out;
 	fftw_plan p;
 	int samples;
+	struct frequency f[100];
+	int num_frequencies = 0;
+	double current = 0, last = 0;
+	int num;
+	int fsamples = 0;
+	int sample;
 
 	memset(&info, 0, sizeof(SF_INFO));
+	memset(&f, 0, sizeof(struct frequency) * 100);
 
-	file = sf_open("untitled.wav", SFM_READ, &info);
+	file = sf_open(argv[1], SFM_READ, &info);
 	if (!file) {
 		printf("Error opening wave file\n");
 		exit(1);
@@ -48,7 +60,7 @@ int main(int argc, char **argv)
 //	plsdev("xwin");
 //	plinit();
 
-//	plenv(0.0f, 0.3f, INT_MIN, INT_MAX, 0, 0);
+//	plenv(0.0f, seconds, -1, 1, 0, 0);
 
 	input = malloc(sizeof(int) * info.channels);
 	if (!input) {
@@ -57,13 +69,15 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	start = 1.8f * (float)info.samplerate;
-	end = 2.1f * (float)info.samplerate;
+	start = 0;
+	end = info.frames;
+//	start = 1.8f * (float)info.samplerate;
+//	end = 2.1f * (float)info.samplerate;
 
 	samples = end - start;
 	in = (double *)fftw_malloc(sizeof(double) * samples);
 	out = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * samples);
-	p = fftw_plan_dft_r2c_1d(samples, in, out, 0);
+	p = fftw_plan_dft_r2c_1d(samples, in, out, FFTW_PRESERVE_INPUT);
 
 	printf("start is %f, end is %f\n", start, end);
 
@@ -92,18 +106,26 @@ int main(int argc, char **argv)
 		total_mean += input[0];
 
 
-		x = input[0];
-		in[i] = x;
+//		x = (double)input[0] / INT_MAX;
+		in[i] = input[0];
 //		plpoin(1, &time, &x, 1);
 	}
 
 //	plend();
 
+	for (i = 0; i < samples; i++) {
+		if (in[i] > 0)
+			in[i] /= max;
+		else if (in[i] < 0)
+			in[i] /= (-min);
+	}
+
 	plsdev("xwin");
 	plinit();
-	plenv(0.0f, ((float)samples / 2) + 1, 0, INT_MAX / 3, 0, 0);
+	plenv(0.0f, ((float)samples / 2) + 1, 0, 200, 0, 0);
 	fftw_execute(p);
 
+	num = 0;
 	for (i = 0; i < samples; i++) {
 		double magnitude;
 		double *d;
@@ -111,10 +133,38 @@ int main(int argc, char **argv)
 		PLFLT y;
 
 		d = out[i];
-		magnitude = hypot(d[0] / 1000.0f, d[1] / 1000.0f);
-		if (magnitude == 0)
+		magnitude = hypot(d[0], d[1]);
+
+		if (magnitude < 10) {
+			num++;
 			continue;
-		printf("d[0]=%f, d[1]=%f, magnitude=%f\n", d[0], d[1], magnitude);
+		}
+
+		if (magnitude < current && num > 100) {
+			f[num_frequencies].magnitude = current;
+			f[num_frequencies].sample = sample;
+			num_frequencies++;
+			num = 0;
+			current = 0;
+			sample = 0;
+		} else if (magnitude < current) {
+			num++;
+		} else if (magnitude > current) {
+			sample = i;
+			current = magnitude;
+		}
+/*
+		if (last && last == current && last > magnitude) {
+			f[num_frequencies].magnitude = current;
+			f[num_frequencies].sample = i - 1;
+			num_frequencies++;
+		} else if (last < magnitude) {
+			current = last;
+		}
+		last = magnitude;
+*/
+//		printf("current=%f, last=%f, magnitude=%f\n", current, last, magnitude);
+//		printf("d[0]=%f, d[1]=%f, magnitude=%f\n", d[0], d[1], magnitude);
 		y = magnitude;
 		plpoin(1, &x, &y, 1);
 	}
@@ -122,6 +172,11 @@ int main(int argc, char **argv)
 	plend();
 	total_mean /= info.frames;
 
+	for (i = 0; i < num_frequencies; i++)
+		printf("Peak %d: magnitude=%f, sample=%d\n",
+		       i, f[i].magnitude, f[i].sample);
+
+	printf("Num frequencies: %d\n", num_frequencies);
 	printf("Min: %f\n", min);
 	printf("Max: %f\n", max);
 	printf("Mean: %f\n", mean);
